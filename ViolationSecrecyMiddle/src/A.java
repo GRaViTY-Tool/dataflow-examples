44aaddbee149b2582707af6aae8ac119f97e7edf
dmebus <dmebus@uni-koblenz.de>
2019-04-03 10:42:25 +0200

    Added example of a mid-flow secrecy violation

---
+++ ViolationSecrecyMiddle/src/A.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * A violation of the secrecy property with the inconsistent part being in the middle of a flow.
+ *
+ */
+public class A {
+	
+	// Secrecy
+	int a = 0;
+	
+	int b = a;
+	
+	// Secrecy
+	int c = b;
+}
